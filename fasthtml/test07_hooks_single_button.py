fad1533d160bae9f310361ba126f73d01dc8a479
Lev Selector <lev.selector@gmail.com>
2024-09-19 15:40:46 -0400

    update

--- fasthtml/test07_hooks_single_button.py
+++ fasthtml/test07_hooks_single_button.py
@@ -12,19 +12,23 @@ app, rt = fast_app()
 def get():
     return Body(
     Script("""function do_upload() {
-                        alert("do upload");
+                        const fileInput = document.getElementById("fileInput");
+                        const file = fileInput.files[0];
+                        alert("do upload file: " + file.name );
                         alert("stop animation");
                 }"""),
     Div(
         H1('Test upload hook'),
         Form(
+            Input(
+                type='file', 
+                name='fileInput', 
+                id='fileInput',
+                hx_post='/upload1', 
+                **{'hx-on:htmx:before-request':"alert('Start');"}, 
+                **{'hx-on:htmx:after-request':"do_upload();"}, 
+                    ),
             Div(id='container'),
-            Button('Start', 
-                   hx_post='/upload1', 
-                   hx_trigger='click', 
-                   **{'hx-on:htmx:before-request':"alert('Start');"}, 
-                   **{'hx-on:htmx:after-request':"do_upload();"}, 
-                   style='margin-left:10px;'),
             enctype='multipart/form-data',
             hx_swap='textContent',
             hx_target='#container'
