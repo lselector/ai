0f3ca857857f68e07ebd80b719e37e516d2b1a2d
Lev Selector <lev.selector@gmail.com>
2024-09-24 08:54:22 -0400

    update

--- demo_RAG/rag_unix/start.bash
+++ demo_RAG/rag_unix/start.bash
@@ -2,9 +2,20 @@
 
 # wrapper to start the demo
 
+if ! python -m myutils &> /dev/null; then
+  echo "Please add absolute path of directory 'ai/py_lib'"
+  echo "to PATH and PYTHOPATH environmental variables"
+  exit
+fi
+
+echo "removing milvus_demo.db"
 rm -rf ./milvus_demo.db
+echo "removing milvus_demo.db.lock"
 rm -rf .milvus_demo.db.lock
+echo "removing uploaded_files/"
 rm -rf uploaded_files/*
 
+echo "starting python script"
 python rag_multi_models.py
+
 
